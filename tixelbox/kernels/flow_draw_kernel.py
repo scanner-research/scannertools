725bcc9e5f9ca53d76d136f33da810a9df4dbaff
Will Crichton <wcrichto@cs.stanford.edu>
2018-04-06 07:07:27 +0000

    Fixed flow vis

--- tixelbox/kernels/flow_draw_kernel.py
+++ tixelbox/kernels/flow_draw_kernel.py
@@ -7,7 +7,8 @@ class FlowDrawKernel(scannerpy.Kernel):
     def execute(self, input_columns):
         [frame, flow] = input_columns
         flow_vis = np.repeat(np.expand_dims(np.average(flow, axis=2), 2), 3, axis=2)
-        combined = np.hstack((frame, flow_vis))
+        combined = np.hstack((frame, (np.clip(flow_vis / np.max(flow_vis), None, 1.0) * 255).astype(
+            np.uint8)))
         return [combined]
 
 
