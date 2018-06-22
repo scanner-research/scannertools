f2cdbb245683cd5077a3517575346a7e895fe87c
Will Crichton <wcrichto@cs.stanford.edu>
2018-06-22 19:27:05 +0000

    Second fix for travis tags

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -3,10 +3,11 @@ set -e
 
 travis-sphinx deploy
 
-if [ -n "$TRAVIS_TAG"]; then
+if [ -n "$TRAVIS_TAG" ];
+then
     docker run -w /app -v $(pwd):/app scannerresearch/scanner:cpu /bin/bash -c "
 pip3 install twine && \
 python3 setup.py bdist_wheel
-twine puload -u 'wcrichto' -p '${PYPI_PASS}' dist/*
+twine upload -u 'wcrichto' -p '${PYPI_PASS}' dist/*
 "
 fi
--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author='Alex Poms and Will Crichton',
     author_email='apoms@cs.stanford.edu',
     license='Apache 2.0',
-    packages=['scannertools', 'scannertools.kernels'],
+    packages=['scannertools'],
     install_requires=['requests', 'numpy', 'scipy', 'requests'],
     zip_safe=False)
