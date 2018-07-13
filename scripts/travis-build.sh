8010f7e11aed03f133a3dd3076f2e359fbcd7c31
Will Crichton <wcrichto@cs.stanford.edu>
2018-07-13 15:07:25 +0000

    Fix pythonpath on travis

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,7 +4,7 @@ set -e
 pushd /tmp
 git clone https://github.com/davidsandberg/facenet
 git clone https://github.com/scanner-research/rude-carnie
-export PYTHONPATH=/tmp/facenet/src/align:/tmp/rude-carnie:$PYTHONPATH
+export PYTHONPATH=/tmp/facenet/src:/tmp/rude-carnie:$PYTHONPATH
 popd
 
 pip3 install travis-sphinx sphinx-nameko-theme pytest tensorflow==1.5.0
