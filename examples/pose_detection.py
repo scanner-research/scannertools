b1da3911b57bf5b48e1fb41e7670fb897cfc4f11
Will Crichton <wcrichto@cs.stanford.edu>
2018-08-11 00:59:13 +0000

    Fixed pose detection example

--- examples/pose_detection.py
+++ examples/pose_detection.py
@@ -5,6 +5,6 @@ import os
 with sample_video() as video:
     db = scannerpy.Database()
     frames = [0, 100, 200]
-    poses = pose_detection.detect_poses(db, video, frames)
-    vis.draw_poses(db, video, poses, frames, path='sample_poses.mp4')
+    poses = pose_detection.detect_poses(db, videos=[video], frames=[frames])
+    vis.draw_poses(db, videos=[video], poses=poses, frames=[frames], path='sample_poses.mp4')
     print('Wrote video with poses drawn to {}'.format(os.path.abspath('sample_poses.mp4')))
