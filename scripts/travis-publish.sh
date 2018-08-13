fb8ac81543e64b0741a66a87d0bf81011811c06d
Will Crichton <wcrichto@cs.stanford.edu>
2018-08-13 18:29:53 +0000

    Fix publish script

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 set -e
 
+# Exit if this is a PR or not the CPU build
+if ! [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TAG" = "cpu"]; then
+    exit 0
+fi
+
+# Push to Pypi if there is a tag on this build
+if [ -n "$TRAVIS_TAG" ];
+then
+    docker run -v $(pwd):/opt/scannertools $DOCKER_REPO:cpu-latest bash -c "
+cd /opt/scannertools
+pip3 install twine
+python3 setup.py bdist_wheel
+twine upload -u 'wcrichto' -p '${PYPI_PASS}' dist/*
+"
+fi
+
+# Exit if it's not the master branch
+if ! ["$TRAVIS_BRANCH" = "master" ]; then
+    exit 0
+fi
+
 pip3 install travis-sphinx
 
 docker run -v $(pwd):/opt/scannertools $DOCKER_REPO:cpu-latest bash -c "
@@ -15,13 +36,3 @@ travis-sphinx build -s doc -n
 "
 
 travis-sphinx deploy
-
-if [ -n "$TRAVIS_TAG" ];
-then
-    docker run -v $(pwd):/opt/scannertools $DOCKER_REPO:cpu-latest bash -c "
-cd /opt/scannertools
-pip3 install twine
-python3 setup.py bdist_wheel
-twine upload -u 'wcrichto' -p '${PYPI_PASS}' dist/*
-"
-fi
