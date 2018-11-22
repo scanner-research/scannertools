35dac11a8fc5bad1898c3c27a67431257c3c9d99
Dan Fu <danfu09@gmail.com>
2018-11-22 16:45:11 +0000

    Face landmark detection

---
+++ scannertools/face_landmark_detection.py
@@ -0,0 +1,68 @@
+from .prelude import Pipeline
+from scannerpy import FrameType, DeviceType
+from scannerpy.stdlib import readers
+import scannerpy
+import pickle
+import numpy as np
+
+# NOTE: This pipeline will *NOT* work out of the box at the moment
+# This pipeline relies on torch > 0.4, but other parts of scannertools rely
+#   on torch = 0.3.1 (clothing detection in particular)
+# Until then, you'll have to manually change the install version 
+
+@scannerpy.register_python_op(name='DetectFaceLandmarksCPU', device_type=DeviceType.CPU)
+@scannerpy.register_python_op(name='DetectFaceLandmarksGPU', device_type=DeviceType.GPU)
+@scannerpy.register_python_op()
+class DetectFaceLandmarks(scannerpy.Kernel):
+    def __init__(self, config):
+        import face_alignment
+
+        self.config = config
+        cpu_only = True
+        for handle in config.devices:
+            if handle.type == DeviceType.GPU.value:
+                cpu_only = False
+                break
+
+        self.fa = face_alignment.FaceAlignment(
+                face_alignment.LandmarksType._2D,
+                device = ('cpu' if cpu_only else 'cuda'),
+                flip_input=False)
+
+    def execute(self, frame: FrameType, bboxes: bytes) -> bytes:
+        [h, w] = frame.shape[:2]
+        bboxes = readers.bboxes(bboxes, self.config.protobufs)
+        if len(bboxes) == 0:
+            return pickle.dumps([])
+
+        # This returns a numpy array of size (68, 2) for every bbox)
+        predictions = self.fa.get_landmarks_from_image(
+                frame,
+                detected_faces=[
+                (bbox.x1 * w,
+                 bbox.y1 * h,
+                 bbox.x2 * w,
+                 bbox.y2 * h) for bbox in bboxes])
+
+        predictions = [
+            np.array([[width / w, height / h]
+                for [width, height] in prediction])
+            for prediction in predictions
+        ]
+
+        return pickle.dumps(predictions)
+
+class FaceLandmarkDetectionPipeline(Pipeline):
+    job_suffix = 'face_landmarks'
+    parser_fn = lambda _: lambda buf, _: pickle.loads(buf)
+    additional_sources = ['bboxes']
+    run_opts = {'pipeline_instances_per_node': 1}
+
+    def build_pipeline(self):
+        return { 'face_landmarks':
+            getattr(self._db.ops, 'DetectFaceLandmarks{}'.format('GPU' if self._db.has_gpu() else 'CPU'))(
+            frame=self._sources['frame_sampled'].op,
+            bboxes=self._sources['bboxes'].op,
+            device=DeviceType.GPU if self._db.has_gpu() else DeviceType.CPU) }
+
+detect_face_landmarks = FaceLandmarkDetectionPipeline.make_runner()
