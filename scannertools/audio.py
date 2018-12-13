beaa2f1b0ecff024a081306ba37454f1c2a3d698
Will Crichton <wcrichto@cs.stanford.edu>
2018-12-13 09:13:12 +0000

    Add duration field to audiosource

--- scannertools/audio.py
+++ scannertools/audio.py
@@ -1,15 +1,19 @@
 from .prelude import *
 
 class AudioSource(DataSource):
-    def __init__(self, video, frame_size=1.0):
+    def __init__(self, video, frame_size=1.0, duration=None):
         self._video = video
         self._frame_size = frame_size
+        self._duration = duration
 
     def scanner_source(self, db):
         return db.sources.Audio(frame_size=self._frame_size)
 
     def scanner_args(self, db):
-        return {'path': self._video.path()}
+        return {
+            'path': self._video.path(),
+            'duration': self._duration if self._duration is not None else 0.0
+        }
 
 class CaptionSource(DataSource):
     def __init__(self, captions, max_time, window_size=10.0):
