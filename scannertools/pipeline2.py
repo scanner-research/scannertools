d4fc404ae583b998e808b6b6e98867e7393a24e0
Will Crichton <wcrichto@cs.stanford.edu>
2019-01-28 02:49:51 +0000

    Next checkpoint

--- scannertools/pipeline2.py
+++ scannertools/pipeline2.py
@@ -4,29 +4,13 @@ from collections import namedtuple, defaultdict
 import scannerpy
 import inspect
 from scannerpy import FrameType
-from typing import Generic, TypeVar, Any
-
-
-@attrs(frozen=True)
-class LazyAccess:
-    obj = attrib()
-    key = attrib()
-
-    def get(self):
-        return getattr(self.obj, self.key)
-
-    def resolve(self):
-        next = self.get()
-        if isinstance(next, LazyAccess):
-            return next.resolve()
-        else:
-            return next
-
-    def __getattr__(self, key):
-        return LazyAccess(obj=self, key=key)
+from typing import Generic, TypeVar, Any, List
 
 
 T = TypeVar('T')
+
+class PerStream(Generic[T]):
+    pass
 
 class Stream(Generic[T]):
     pass
@@ -34,107 +18,185 @@ class Stream(Generic[T]):
 class Sink(Generic[T]):
     pass
 
-class Input:
-    def __init__(self, ty, default=None):
-        self._type = ty
-        self._default = default
+class IO:
+    pass
 
-class Output:
-    def __init__(self, ty):
-        self._type = ty
+@attrs(frozen=True)
+class Input(IO):
+    type = attrib()
+    default = attrib(default=None)
 
-class OpGraphMeta(type):
-    def __init__(cls, name, bases, ns):
-        super(OpGraphMeta, cls).__init__(name, bases, ns)
-        cls._output_keys = [k for k, v in vars(cls).items() if isinstance(v, Output)]
+@attrs(frozen=True)
+class Output(IO):
+    type = attrib()
 
-class OpGraph(metaclass=OpGraphMeta):
-    def __init__(self, *args, **kwargs):
-        self._build_args = (args, kwargs)
-        self._built = False
+@attrs(frozen=True)
+class BoundVar:
+    cls = attrib()
+    var = attrib()
+    key = attrib()
 
-    def __getattribute__(self, k):
-        output_keys = super(OpGraph, self).__getattribute__('_output_keys')
-        if k in output_keys:
-            return LazyAccess(self, k)
+    def resolve(self):
+        if isinstance(self.var, Input):
+            ty = self.var.type
+            cls_args = self.cls.args()
+            if Stream in ty.__mro__:
+                return cls_args[self.key].resolve()
+            elif PerStream in ty.__mro__:
+                if self.key in cls_args:
+                    return cls_args[self.key].resolve()
+                else:
+                    return self.cls._per_stream[self.key]
+            else:
+                raise Exception(ty)
+        elif isinstance(self.var, Output):
+            return self.cls._outputs[self].resolve()
         else:
-            return super(OpGraph, self).__getattribute__(k)
+            raise Exception("Unreachable")
+
+class OpGraph():
+    def __init__(self, **kwargs):
+        for k, v in vars(type(self)).items():
+            if isinstance(v, IO):
+                setattr(self, k, BoundVar(cls=self, key=k, var=v))
+
+        self._args = kwargs
+        self._expanded = False
+
+    def args(self):
+        return self._args
 
     def initialize(self, db):
         self.db = db
 
-    def _build(self, globals):
-        if self._built:
+    def op_param_to_op(self, op):
+        if isinstance(op, OpLeaf):
+            return op
+        else:
+            return op.cls
+
+    def expand_graph(self):
+        if self._expanded:
             return
         else:
-            self._built = True
+            self._expanded = True
 
-        (args, kwargs) = self._build_args
-        sig = inspect.signature(self.build)
-        for param, ty in sig.parameters.items():
-            ty = ty.annotation
-            if Stream in ty.__mro__:
-                kwargs[param] = kwargs[param].get()
+        outputs = self.build()
+        self._outputs = outputs
 
-        my_globals = {k.key: v for k, v in globals.items() if k.obj == self}
-        bindings = self.build(*args, **kwargs, **my_globals)
-        self._bindings = bindings
-
-        #nodes = self._search_and_toposort(aliases)
-        nodes = list(self._bindings.values())
-        print(nodes)
-
+        nodes = self.graph_search(outputs.values())
+        edges = set()
         for node in nodes:
-            if isinstance(node, OpLeaf):
-                graph = node
-            elif not isinstance(node, LazyAccess):
-                raise Exception("Object {} is not a LazyAccess".format(node))
-            else:
-                graph = node.obj
+            for other_node in node.args().values():
+                if isinstance(other_node, BoundVar) and isinstance(other_node.var, Output):
+                    edges.add((self.op_param_to_op(other_node), node))
 
-            graph.initialize(db)
-            graph._build({
-                LazyAccess(k.obj.get(), k.key): v
-                for k, v in globals.items() if isinstance(k.obj, LazyAccess) and k.obj.get() == node
-            })
+        sorted_nodes = self.toposort(nodes, edges)
 
-        return type(aliases)(**{k: v.get() if isinstance(v, LazyAccess) else v for k, v in aliases._asdict().items()})
+        for node in sorted_nodes:
+            node.initialize(self.db)
+            node.expand_graph()
 
-    def _search_and_toposort(self, aliases):
-        # TODO
+        return outputs
+
+    def graph_search(self, ops):
+        # Breadth-first search
+        frontier = set(ops)
+        visited = set()
+        while len(frontier) > 0:
+            visited |= frontier
+            new_frontier = set()
+            for node in frontier:
+                for possible_node in self.op_param_to_op(node).args().values():
+                    if isinstance(possible_node, BoundVar) and isinstance(possible_node.var, Output):
+                        new_frontier.add(possible_node)
+            frontier = new_frontier
+
+        return {self.op_param_to_op(bound_output) for bound_output in visited}
+
+    # https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+    def toposort(self, nodes, edges):
+        sorted_list = []
+
+        # Make a copy of edges so we can delete from it
+        edges = list(edges)
+
+        # Initialize with nodes that have no incoming edges
+        to_visit = [node for node in nodes
+                    if all([edge[1] != node for edge in edges])]
+
+        while len(to_visit) > 0:
+            parent = to_visit.pop()
+            sorted_list.append(parent)
+
+            for child in nodes:
+                to_child_edges = [i for i, edge in enumerate(edges)
+                                  if edge[0] == parent and edge[1] == child]
+
+                if len(to_child_edges) == 0:
+                    continue
+
+                # Remove edges in reverse order to preserve correct indices
+                for i in to_child_edges[::-1]:
+                    del edges[i]
+
+                if all([edge[1] != child for edge in edges]):
+                    to_visit.append(child)
+
+        if len(edges) > 0:
+            raise Exception("Block graph contains a cycle")
+
+        return sorted_list
+
+    def job_args(self):
         pass
 
-    def run(self, db, outputs, per_video, globals):
-        aliases = self._build(globals)
-        outputs = [node.resolve() for node in outputs]
-        print(outputs)
+    def build_jobs(self, per_stream_args):
+        per_stream_arg_names = \
+            [k for k, v in vars(type(self)).items()
+             if isinstance(v, Input) and PerStream in v.type.__mro__]
 
-class OpLeaf:
-    def __init__(self, f, args=[]):
+        var_to_str_map = {v.key: v for v in per_stream_args[0].keys()}
+
+        jobs = []
+        for args in per_stream_args:
+            named_args = {name: args[var_to_str_map[name]] for name in per_stream_arg_names}
+            op_args = {k.resolve(): v for k, v in self.job_args(**named_args).items()}
+            jobs.append(scannerpy.Job(op_args=op_args))
+
+        return jobs
+
+    def run(self, db, outputs, per_stream, globals={}):
+        self._per_stream = per_stream
+        self.initialize(db)
+        self.expand_graph()
+
+        output_ops = [node.resolve() for node in outputs]
+        assert(len(output_ops) == 1)
+
+        jobs = self.build_jobs(per_stream)
+        db.run(output_ops[0], jobs)
+
+class OpLeaf(OpGraph):
+    def __init__(self, f, args={}):
         self._f = f
         self._args = args
 
-    def initialize(self, db):
-        self.db = db
+    def args(self):
+        return self._args
 
-    def _build(self, globals):
-        for node in self._args:
-            if isinstance(node, OpLeaf):
-                graph = node
-            elif not isinstance(node, LazyAccess):
-                raise Exception("Object {} is not a Node".format(node))
-            else:
-                graph = node.obj
+    def expand_graph(self):
+        for key, node in self._args.items():
+            self.op_param_to_op(node).expand_graph()
 
-            graph.initialize(db)
-            graph._build({})
+        self.val = self._f(**{key: node.resolve() for key, node in self._args.items()})
 
-        op = self._f(*[node.get() if isinstance(node, Node) else node._op for node in self._args])
-        self._op = op
-        return op
+    def resolve(self):
+        return self.val
 
 
 class ScannerFrameSource(OpGraph):
+    video = Input(PerStream[Video])
     frame = Output(Stream[FrameType])
 
     def build(self):
@@ -142,73 +204,91 @@ class ScannerFrameSource(OpGraph):
             self.frame: OpLeaf(lambda: self.db.sources.FrameColumn())
         }
 
+    def job_args(self, video):
+        return {self.frame: self.db.table(video.scanner_name()).column('frame')}
+
+
 class Gather(OpGraph):
     input_stream = Input(Stream[Any])
+    indices = Input(PerStream[List[int]])
     output_stream = Output(Stream[Any])
 
     def build(self):
         return {
             self.output_stream: OpLeaf(lambda stream: self.db.streams.Gather(stream),
-                                       [self.input_stream])
+                                       {'stream': self.input_stream})
         }
 
+    def job_args(self, indices):
+        return {self.output_stream: indices}
+
 class GatheredFrame(OpGraph):
+    video = Input(PerStream[Video])
+    indices = Input(PerStream[List[int]])
     frame = Output(Stream[FrameType])
     stream = Output(Stream[FrameType])
 
     def build(self):
-        frame = ScannerFrameSource()
-        gather = Gather(stream=frame.frame)
+        self._frame_source = ScannerFrameSource(video=self.video)
+        self._gather = Gather(input_stream=self._frame_source.frame)
         return {
-            self.frame: frame.frame,
-            self.stream: gather.stream
+            self.frame: self._frame_source.frame,
+            self.stream: self._gather.output_stream
         }
+
+    def job_args(self, video, indices):
+        return {**self._frame_source.job_args(video=video), **self._gather.job_args(indices=indices)}
 
 class Histogram(OpGraph):
     frame = Input(Stream[FrameType])
-    bins = Input(int, 16)
     histogram = Output(Stream[bytes])
 
     def build(self):
-        print(self.bins)
         return {
             self.histogram: OpLeaf(lambda frame: self.db.ops.Histogram(frame=frame),
-                                   [self.frame])
+                                   {'frame': self.frame})
         }
 
 class ScannerColumnSink(OpGraph):
     column = Input(Stream[Any])
-    sink = Input(Sink[Any])
+    table_name = Input(PerStream[str])
+    sink = Output(Sink[Any])
 
-    def build(self, column: Stream[bytes]):
+    def build(self):
         return {
             self.sink: OpLeaf(lambda column: self.db.sinks.Column(columns={'column': column}),
-                              [self.column])
+                              {'column': self.column})
         }
+
+    def job_args(self, table_name):
+        return {self.sink: table_name}
+
 
 class HistogramPipeline(OpGraph):
-    video = Output(Stream[FrameType])
-    frames = Output(Stream[FrameType])
-    histogram = Output(Stream[bytes])
+    video = Input(PerStream[Video])
+    table_name = Input(PerStream[str])
+    indices = Input(PerStream[List[int]])
     table = Output(Sink[bytes])
 
-    def build(self, test: int = 1):
-        frame = GatheredFrame()
-        histogram = Histogram(frame=frame.stream)
-        sink = ScannerColumnSink(column=histogram.histogram)
+    def build(self):
+        self._gathered_frame = GatheredFrame(video=self.video)
+        histogram = Histogram(frame=self._gathered_frame.stream)
+        self._sink = ScannerColumnSink(column=histogram.histogram, table_name=self.table_name)
+        return {self.table: self._sink.sink}
+
+    def job_args(self, video, indices, table_name):
         return {
-            self.video: frame.frame,
-            self.frames: frame.stream,
-            self.histogram: histogram.histogram,
-            self.table: sink.column
+            **self._gathered_frame.job_args(video=video, indices=indices),
+            **self._sink.job_args(table_name=table_name)
         }
+
 
 from .prelude import sample_video
 with sample_video(delete=False) as video:
     db = scannerpy.Database()
     p = HistogramPipeline()
-    p.run(
+    result = p.run(
         db,
         outputs=[p.table],
-        per_video=[{p.video: video, p.table: "test"}],
-        globals={p.histogram.bins: 8})
+        per_stream=[{p.video: video, p.table_name: "test", p.indices: [0]}])
+        #globals={p.histogram.bins: 8})
